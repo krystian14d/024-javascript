a02028928990af2d79dcc108c45aad4f2656324a
krystianluc <krystian.luc@q-perior.com>
2023-09-28 18:21:20 +0200

    basics

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,14 @@
 const task3Element = document.getElementById('task-3');
+
+function first(){
+    alert('Some text');
+}
+
+function second(text){
+    alert(text);
+}
+
+first();
+second('Second function');
+
+task3Element.addEventListener('click', first);
