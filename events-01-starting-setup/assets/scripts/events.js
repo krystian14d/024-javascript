7de711724222def84baf7a11fd8f4ec308b6b977
Krystian <krystian14d@gmail.com>
2023-10-22 17:08:24 +0200

    events

---
+++ events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,43 @@
+const button = document.querySelector('button');
+
+const buttonClickHandler = event => {
+  // event.target.disabled = true;
+  console.log(event);
+}
+
+const boundFunction = buttonClickHandler.bind(this);
+
+// button.addEventListener('click', buttonClickHandler);
+
+// setTimeout(() => {
+//   button.removeEventListener('click', buttonClickHandler);
+// }, 3000)
+
+// buttons.forEach(btn => {
+//   btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {
+//   console.log(event);
+// });
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector('div');
+
+div.addEventListener('click', event => {
+  console.log('CLICKED DIV');
+  console.log(event);
+}, true);
+
+button.addEventListener('click', event => {
+  event.stopPropagation();
+  console.log('CLICKED BUTTON');
+  console.log(event);
+});
