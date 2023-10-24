46cdc799b3da36b0472648523011bad9626402e2
krystianluc <krystian.luc@q-perior.com>
2023-10-24 16:45:50 +0200

    events

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('button');
 
-const buttonClickHandler = event => {
+const buttonClickHandler = (event) => {
   // event.target.disabled = true;
   console.log(event);
-}
+};
 
 const boundFunction = buttonClickHandler.bind(this);
 
@@ -21,23 +21,43 @@ const boundFunction = buttonClickHandler.bind(this);
 //   console.log(event);
 // });
 
-
 const form = document.querySelector('form');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log(event);
 });
 
 const div = document.querySelector('div');
 
-div.addEventListener('click', event => {
-  console.log('CLICKED DIV');
-  console.log(event);
-}, true);
+div.addEventListener(
+  'click',
+  (event) => {
+    console.log('CLICKED DIV');
+    console.log(event);
+  },
+  true
+);
 
-button.addEventListener('click', event => {
+button.addEventListener('click', (event) => {
   event.stopPropagation();
   console.log('CLICKED BUTTON');
   console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   })
+// });
+
+const list = document.querySelector('ul');
+
+list.addEventListener('click', (event) => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
+  form.submit();
 });
