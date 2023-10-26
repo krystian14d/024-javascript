67783ab29d7367ef82ec717701a1e575e824f2ce
krystianluc <krystian.luc@q-perior.com>
2023-10-26 15:28:35 +0200

    HTTP requests

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -19,32 +19,58 @@ const getPosition = (opts) => {
 const setTimer = (duration) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve('Done!');
+      resolve('Timer done!');
     }, duration);
   });
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-  .then((posData) => {
-    positionData = posData;
-    // return setTimer(2000);
-  })
-  .catch(err => {
-    console.log(err);
-  })
-  .then(data => {
-    console.log(data, positionData);
-  });
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  });
-  console.log('Getting position...');
+async function trackUserHandler() {
+  // let positionData;
+  let posData;
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000);
+  } catch (error) {
+    console.log(timerData, posData);
+  }
+  // getPosition()
+  // .then((posData) => {
+  //   positionData = posData;
+  //   return setTimer(2000);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   return 'there was an error...';
+  // })
+  // .then(data => {
+  //   console.log(data, positionData);
+  // });
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // });
+  // console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
+
+//zwroci do .then() wynik tej funkcji, która się szybciej wykona:
+Promise.race([getPosition(), setTimer(1000)]).then(data => {
+  console.log(data);
+}); 
+
+//zwraca promise jako rezultat, ale wynikiem są połączone dane Promises przekazanych w tablicy
+//albo wszystkie się wykonają (resolved), albo przynajmniej jedna promise jest rejected
+Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+}); 
+
+//zwraca promise jako rezultat, ale wynikiem są połączone dane Promises przekazanych w tablicy
+//zwraca wynik wszystkich przekazanych promises, nie ważne czy zakończyły się powodzeniem czy błędem
+Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+  console.log(promiseData);
+}); 
 
 // let result = 0;
 
