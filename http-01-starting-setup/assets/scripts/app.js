7876b7ef7dcb9fc5df7b790d952303d60bccd111
krystianluc <krystian.luc@q-perior.com>
2023-10-26 20:36:42 +0200

    HTTP requests

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('new-post');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
@@ -31,6 +32,7 @@ function fetchPosts(){
           const postEl = document.importNode(postTemplate.content, true);
           postEl.querySelector('h2').textContent = post.title.toUpperCase();
           postEl.querySelector('p').textContent = post.body;
+          postEl.querySelector('li').id = post.id;
           listElement.append(postEl);
         }
     })
@@ -53,4 +55,11 @@ form.addEventListener('submit', event => {
     const enteredTitle = event.currentTarget.querySelector('#title').value;
     const enteredContent = event.currentTarget.querySelector('#content').value;
     createPost(enteredTitle, enteredContent);
-})
+});
+
+postList.addEventListener('click', event => {
+    if(event.target.tagName === 'BUTTON'){
+        const postId = event.target.closest('li').id;
+        console.log(postId);
+    }
+});
